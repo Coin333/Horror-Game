2815be0412f33183959245944d0c2ecac03d5daa
Stephen-Hawking <166663046+Stephen-Hawking@users.noreply.github.com>
2025-02-12 09:59:23 -0800

    h

--- scenes/main_character.gd
+++ scenes/main_character.gd
@@ -1,13 +1,17 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 250.0
+@onready var sprite_2d = $Sprite2D
 
 
+func _physics_process(delta):
 
-func _physics_process(delta: float) -> void:
-
-
+	#Animations
+	if (velocity.x > 1 || velocity.x < -1):
+		sprite_2d.animation = "walk_left"
+	else:
+		sprite_2d.animation = "walk_down"
 
 
 	# Get the input direction and handle the movement/deceleration.
@@ -25,3 +29,7 @@ func _physics_process(delta: float) -> void:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 	move_and_slide()
 	
+	var isLeft = velocity.x < 0
+	sprite_2d.flip_h = isLeft
+	
+#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
